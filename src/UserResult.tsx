4f275369307d1a36c24d077342e943b3bd5adb90
Star <67430892+alicenstar@users.noreply.github.com>
2021-04-23 16:51:29 -0500

    Begin adding styling to user results

--- src/UserResult.tsx
+++ src/UserResult.tsx
@@ -2,7 +2,22 @@ import Paper from '@material-ui/core/Paper';
 import Avatar from '@material-ui/core/Avatar';
 import React from 'react';
 import { IUser } from './Search';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core';
 
+
+const useStyles = makeStyles({
+    root: {
+        minHeight: '80px',
+        padding: '16px'
+    },
+    avatars: {
+        width: '80px',
+        height: '80px',
+        float: 'left',
+        marginRight: '14px'
+    }
+});
 
 interface IUserResultProps {
     users: IUser[];
@@ -12,14 +27,36 @@ export const UserResult: React.FC<IUserResultProps> = ({
     users
 }: IUserResultProps) => {
 
+    const classes = useStyles();
+
     return (
         <div>
             {users.map((user: IUser) => (
-                <Paper elevation={2}>
+                <Paper
+                 className={classes.root}
+                 elevation={2}
+                >
                     <Avatar
-                    alt={`${user.node.name}` + 'avatar'}
-                    src={`${user.node.avatarUrl}`}
+                     className={classes.avatars}
+                     alt={`${user.node.name}'s avatar`}
+                     src={`${user.node.avatarUrl}`}
                     />
+                    <Typography
+                     variant="h6"
+                     display="inline"
+                    >
+                        {user.node.name}
+                    </Typography>
+                    <Typography
+                     variant="subtitle1"
+                    >
+                        {user.node.login}
+                    </Typography>
+                    <Typography
+                     variant="subtitle2"
+                    >
+                        {user.node.bio}
+                    </Typography>
                 </Paper>
             ))}
         </div>
